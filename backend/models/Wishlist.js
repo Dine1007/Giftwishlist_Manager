5a2fb1877eb0eb7e71d66d36c4195fe9ee6dfe3a
Dinethra <dinethra1234@gmail.com>
2026-04-03 02:30:35 +1000

    Backend development of sharing a wishlist with peers

--- backend/models/Wishlist.js
+++ backend/models/Wishlist.js
@@ -4,9 +4,16 @@ const crypto = require('crypto');
 const wishlistSchema = new mongoose.Schema({
   name: { type: String, required: true },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  shareLink: { type: String, unique: true },
  
 }, { timestamps: true });
 
-
+// Auto-generate unique share link before saving
+wishlistSchema.pre('save', function (next) {
+  if (!this.shareLink) {
+    this.shareLink = crypto.randomBytes(8).toString('hex');
+  }
+  next();
+});
 
 module.exports = mongoose.model('Wishlist', wishlistSchema);
